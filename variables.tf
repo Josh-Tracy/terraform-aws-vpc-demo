585414975596f432fdb7c5bea8719f2799f72a58
Joshua Tracy <55331456+Josh-Tracy@users.noreply.github.com>
2023-06-29 12:03:32 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -14,21 +14,6 @@ variable "subnet_delta_cidr_block" {
   description = "The CIDR block for the delta subnet"
 }
 
-variable "role_arn" {
-  type        = string
-  description = "The ARN of the role to assume"
-}
-
-variable "external_id" {
-  type        = string
-  description = "The External ID to provide for the role you are assuming"
-}
-
-variable "session_name" {
-  type        = string
-  description = "The session name for the role you are assuming"
-}
-
 variable "tag_environment" {
   type        = string
   description = "A default tag applied to all resources. Environment name the resources are deployed into"
